00d60fe824ccc864fb3421af447f8323972104df
jytrowbridge <jytrowbridge@gmail.com>
2018-11-15 21:47:17 -0500

    Fix bad quotation marks

--- install.bat
+++ install.bat
@@ -9,7 +9,7 @@ set "dir=%dir:\=/%"
 @echo ico_dir = "%dir%/lib/" >> lib\config.py
 
 :: create exe:
-pyinstaller.exe --windowed --"icon=%cd%\lib\jack.ico" ""%cd%\lib\parser.py"
+pyinstaller.exe --windowed --icon="%cd%\lib\jack.ico" "%cd%\lib\parser.py"
 
 :: create shortcuts:
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Fixed Width Parser.lnk');$s.TargetPath='%cd%\dist\parser\parser.exe';$s.Save()"
