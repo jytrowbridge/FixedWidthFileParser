77d3009f5f2969a929fe0c4e105fe140bd7d399c
jytrowbridge1 <36171816+jytrowbridge1@users.noreply.github.com>
2018-11-15 19:01:50 -0500

    Clean up install script and fix icon issues

---
+++ lib/config.py
@@ -0,0 +1,2 @@
+# stores directory where parser is installed; used to point to icon files.  This variable is stored during install
+ico_dir = "C:/Users/jack.trowbridge/Documents/Allied/FixedWidthFileParser/lib/"
