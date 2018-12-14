a28f55128942b10308d8a798943ebb585142da45
jytrowbridge <jytrowbridge@gmail.com>
2018-12-13 20:55:01 -0500

    clean config file

--- lib/config.py
+++ lib/config.py
@@ -1,2 +1,1 @@
 # stores directory where parser is installed; used to point to icon files.  This variable is stored during install
-ico_dir = "C:/Users/jack.trowbridge/Documents/Allied/FixedWidthFileParser/lib/"
